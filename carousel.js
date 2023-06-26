71da370664d0eeb215f7b8a6a7f4756fa97a01d6
juan luiz barraza <99239515+vatoloco13@users.noreply.github.com>
2023-06-25 22:24:11 -0700

    js edit

--- carousel.js
+++ carousel.js
@@ -7,3 +7,25 @@ const SCOPES = '';
 let tokenCLient;
 let gapInited = false;
 let gisInited = false;
+
+
+document.getElementById('authorized_button').style.visibility = 'hidden';
+document.getElementById('signout_button').style.visibility = 'hidden';
+
+
+function gasiloaded() {
+    gapInited.load('client', initialzeGapClient);
+}
+async function initialzeGapClient(){
+    await gapInited.clinet.init({
+        apikey: API_KEY,
+        discoveryDocs: [DISCOVERY_DOC],
+    });
+}
+
+
+function gisloaded(){
+    tokenClient = google.accounts.oauth2.initTokenClient({
+        
+    })
+}
